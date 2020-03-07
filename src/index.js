0d7e9584d2a1251bbec5159110109b2258071160
chalpenny <cp.halpenny@gmail.com>
2020-03-07 12:48:53 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById('create-task-form').addEventListener('submit', function (event) { 
     event.preventDefault();
     let formInput = document.getElementById('new-task-description');
+    console.log("formInput", formInput.value)
     tasks.push(formInput.value);
+    
+
   
       li = document.createElement("li");
-      li.innerHTML = formInput.value
-      list.appendChild(li)
-  });
+      li.innerHTML = formInput.value;
+      list.appendChild(li);
+      formInput.value = '';  });
 });
